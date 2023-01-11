ff9313c70972cb8cf61715e01d410e105e6ef77d
chenxuuu <lolicon@papapoi.com>
2023-01-11 14:24:59 +0800

    update: 灯全改掉

--- source/hic_hal/stm32/stm32f103xb/IO_Config.h
+++ source/hic_hal/stm32/stm32f103xb/IO_Config.h
@@ -67,20 +67,20 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F103XB);
 //LEDs
 //USB status LED
 #define RUNNING_LED_PORT             GPIOA
-#define RUNNING_LED_PIN              GPIO_PIN_9
-#define RUNNING_LED_Bit              9
+#define RUNNING_LED_PIN              GPIO_PIN_0
+#define RUNNING_LED_Bit              0
 
 #define PIN_HID_LED_PORT             GPIOA
-#define PIN_HID_LED                  GPIO_PIN_9
-#define PIN_HID_LED_Bit              9
+#define PIN_HID_LED                  GPIO_PIN_0
+#define PIN_HID_LED_Bit              0
 
 #define PIN_CDC_LED_PORT             GPIOA
-#define PIN_CDC_LED                  GPIO_PIN_9
-#define PIN_CDC_LED_Bit              9
+#define PIN_CDC_LED                  GPIO_PIN_0
+#define PIN_CDC_LED_Bit              0
 
 #define PIN_MSC_LED_PORT             GPIOA
-#define PIN_MSC_LED                  GPIO_PIN_9
-#define PIN_MSC_LED_Bit              9
+#define PIN_MSC_LED                  GPIO_PIN_0
+#define PIN_MSC_LED_Bit              0
 
 
 #endif
